701e34fe55080bfc8ecf5fa66d2aba9bba7dab69
arnasnavikas <arnasnavikas@yahoo.com>
2017-03-08 11:12:48 +0200

    added ip to paths.js

--- routes/paths.js
+++ routes/paths.js
@@ -5,12 +5,14 @@ var localhost = {
     private_images_folder   : '/home/arnas/nodeJS/my-app/backend/public/private_images/',
     public_folder   : '/home/arnas/nodeJS/my-app/backend/public/',
     images_location : 'http://localhost:3000/',
-    private_images_location  : 'http://localhost:3000/private_images/'
+    private_images_location  : 'http://localhost:3000/private_images/',
+    ipAddress: null
 };
 var realServer = {
-    private_images_folder   : '/home/deploy/my_site/AppBackend/public/private_images',
+    private_images_folder   : '/home/deploy/my_site/AppBackend/public/private_images/',
     public_folder   : '/home/deploy/my_site/AppBackend/public/',
     images_location : 'http://46.101.120.14:3000/',
-    private_images_location  : 'http://46.101.120.14:3000/private_images/'
+    private_images_location  : 'http://46.101.120.14:3000/private_images/',
+    ipAddress: '46.101.120.14'
 }
 module.exports = localhost;
